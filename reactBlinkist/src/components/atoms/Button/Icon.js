385f48d15c53daf36ac79fa0093e0b728bd18a8b
NisanthChsr <55597238+NisanthChsr@users.noreply.github.com>
2021-05-24 22:51:05 +0530

    Update Icon.js

--- reactBlinkist/src/components/atoms/Button/Icon.js
+++ reactBlinkist/src/components/atoms/Button/Icon.js
@@ -4,7 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 const TextButtons = ({onClick,endIcon}) => {
 
   return (
-      <IconButton onClick={onClick}  >
+      <IconButton role='icon' onClick={onClick}  >
           {endIcon}
       </IconButton>
   );
