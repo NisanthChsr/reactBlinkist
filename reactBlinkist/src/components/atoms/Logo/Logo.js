025fa5555d93ab8b9fa30a7e29e5ab5eab1c05a3
NisanthChsr <55597238+NisanthChsr@users.noreply.github.com>
2021-05-24 20:25:39 +0530

    Delete reactBlinkist/src/components/atoms directory

--- reactBlinkist/src/components/atoms/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import { Avatar } from '@material-ui/core';
-import Blinklist from './Blinklist.png';
-import './style.css';
-
-export default function Logo(){
-    return (
-           <img className='logo' src={Blinklist} alt='Blinklist' />
-    );
-}
--- reactBlinkist/src/components/atoms/Logo/Logo.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Logo from './Logo'
-export default {
-  title: 'Atoms/Logo'
-}
-
-export const Primary = () => <Logo>Logo</Logo>
